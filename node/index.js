21f5728dae343a11a3e7c762aeee4fee9b876154
Rafael Vilarino <rovilarino@gmail.com>
2023-03-06 12:39:45 -0300

    adicionando lógica dentro do index.js

--- node/index.js
+++ node/index.js
@@ -12,6 +12,13 @@ const mysql = require('mysql')
 
 app.get('/', (req, res) => {
     const connection = mysql.createConnection(config)
+    connection.query("DROP TABLE IF EXISTS people;")
+    connection.query("CREATE TABLE people( id int not null auto_increment,  name varchar(255), primary key(id));")
+
+    connection.query("INSERT INTO people (name) VALUES ('Rafael')")
+    connection.query("INSERT INTO people (name) VALUES ('Pedro')")
+    connection.query("INSERT INTO people (name) VALUES ('Tiago')")
+
     const sql = `SELECT * FROM people`
     connection.query(sql, (err, rows) => {
          
